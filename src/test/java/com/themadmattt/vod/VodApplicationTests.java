bbad16e34195451e54e2f783ed1af029889f1dfc
Mateusz Polok <matipolok@gmail.com>
2019-01-15 14:49:55 +0100

    init project

---
+++ src/test/java/com/themadmattt/vod/VodApplicationTests.java
@@ -0,0 +1,17 @@
+package com.themadmattt.vod;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class VodApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
